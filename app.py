965e1fe6c22b5b6ef08dac042c4ac64d8d1a41fd
mauro <mrb0305@gmail.com>
2025-10-17 16:20:14 -0300

    Read and Find

--- app.py
+++ app.py
@@ -15,6 +15,19 @@ def create_task():
     tasks.append(new_task)
     return jsonify({"message": "Task created"})
 
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    tasks_dict = [task.to_dict() for task in tasks]
+    output = jsonify({"tasks": tasks_dict,"total": len(tasks_dict)})
+    return output
+
+@app.route('/tasks/<int:task_id>', methods=['GET'])
+def get_task_(task_id):
+    for task in tasks:
+        if task.id == task_id:
+            return jsonify(task.to_dict())
+    return jsonify({"message": "Task not found"}), 404
+
 debugando = __name__ == '__main__'
 app.run(debug=debugando)
 

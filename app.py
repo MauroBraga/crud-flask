140a8543bcebdd6e0a41e9275cba374519c4e642
mauro <mrb0305@gmail.com>
2025-10-18 11:28:16 -0300

    Delete

--- app.py
+++ app.py
@@ -28,6 +28,31 @@ def get_task_(task_id):
             return jsonify(task.to_dict())
     return jsonify({"message": "Task not found"}), 404
 
+
+@app.route('/tasks/<int:task_id>', methods=['PUT'])
+def update_task(task_id):
+    data = request.get_json()
+    for task in tasks:
+        if task.get_id == task_id:
+            task._Task__title = data["title"]
+            task._Task__description = data["description"]
+            task._Task__completed = data["completed"]
+            return jsonify({"message": "Task updated"})
+    return jsonify({"message": "Task not found"}), 404
+
+@app.route('/tasks/<int:task_id>', methods=['DELETE'])
+def delete_task(task_id):
+    taskDeleted = None
+    for task in tasks:
+        if task.get_id == task_id:
+            taskDeleted = task
+            break
+    if taskDeleted:
+        tasks.remove(taskDeleted)
+        return jsonify({"message": "Task deleted"})
+
+    return jsonify({"message": "Task not found"}), 404
+
 debugando = __name__ == '__main__'
 app.run(debug=debugando)
 
